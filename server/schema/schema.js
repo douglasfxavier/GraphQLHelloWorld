181fa1eecf1a31003d4658da41f037dc1c5831c4
Douglas Xavier <douglasf.xavier@gmail.com>
2019-06-17 13:45:43 -0300

    Inclui server

---
+++ server/schema/schema.js
@@ -0,0 +1,29 @@
+const graphql = require('graphql');
+const {
+        GraphQLObjectType,
+        GraphQLSchema,
+        GraphQLString
+} = graphql;
+
+const helloWorldType = new GraphQLObjectType({
+    name: 'HelloWorld',
+    fields: ()=>({
+        texto: {type: GraphQLString}
+    })
+});
+
+const queryType = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        helloword: {
+            type: helloWorldType,
+            resolve(){
+                return {texto: "Hello World"};
+            }
+        }
+   }
+});
+
+module.exports = new GraphQLSchema({
+    query:queryType
+})
